f88c9093a853375efe8ee192c8b4abf6b98c7203
Jakub Łanecki <jakub.lanecki@relia-sol.pl>
2020-03-22 19:42:02 +0100

    Messy (but working\!) loopback video stream with WebRTC.

--- mobile/app/src/main/java/com/example/deepnoise/CallActivity.kt
+++ mobile/app/src/main/java/com/example/deepnoise/CallActivity.kt
@@ -1,17 +1,128 @@
 package com.example.deepnoise
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.deepnoise.databinding.ActivityCallBinding
+import kotlinx.android.synthetic.main.activity_call.*
+import org.webrtc.*
 
 class CallActivity : AppCompatActivity() {
 
+    companion object {
+        private const val CAMERA_PERMISSION_REQUEST_CODE = 1
+        private const val CAMERA_PERMISSION = Manifest.permission.CAMERA
+    }
+
+    private lateinit var binding: ActivityCallBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityCallBinding.inflate(layoutInflater)
+        binding = ActivityCallBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val calleeKey = resources.getString(R.string.callee)
         binding.callee.text = intent.getStringExtra(calleeKey)
+
+        checkCameraPermission()
+    }
+
+    private fun checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, CAMERA_PERMISSION) != PackageManager.PERMISSION_GRANTED) {
+            requestCameraPermission()
+        } else {
+            onCameraPermissionGranted()
+        }
+    }
+
+    private fun onCameraPermissionGranted() {
+        doStuff()
+    }
+
+    private fun requestCameraPermission(dialogShown: Boolean = false) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, CAMERA_PERMISSION) && !dialogShown) {
+            showPermissionRationaleDialog()
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(CAMERA_PERMISSION), CAMERA_PERMISSION_REQUEST_CODE)
+        }
+    }
+
+    private fun showPermissionRationaleDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Camera Permission Required")
+            .setMessage("This app need the camera to function")
+            .setPositiveButton("Grant") { dialog, _ ->
+                dialog.dismiss()
+                requestCameraPermission(true)
+            }
+            .setNegativeButton("Deny") { dialog, _ ->
+                dialog.dismiss()
+                onCameraPermissionDenied()
+            }
+            .show()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+            onCameraPermissionGranted()
+        } else {
+            onCameraPermissionDenied()
+        }
+    }
+
+    private fun onCameraPermissionDenied() {
+        Toast.makeText(this, "Camera Permission Denied", Toast.LENGTH_LONG).show()
+    }
+
+    private fun doStuff() {
+        // Initialize PeerConnectionFactory options.
+        val options = PeerConnectionFactory.InitializationOptions.builder(this)
+            .setEnableInternalTracer(true)
+            .setFieldTrials("WebRTC-H264HighProfile/Enabled/")
+            .createInitializationOptions()
+        PeerConnectionFactory.initialize(options)
+
+        // Configure the PeerConnectionFactory builder.
+        val rootEglBase: EglBase = EglBase.create()
+        val peerConnectionFactory = PeerConnectionFactory
+            .builder()
+            .setVideoDecoderFactory(DefaultVideoDecoderFactory(rootEglBase.eglBaseContext))
+            .setVideoEncoderFactory(DefaultVideoEncoderFactory(rootEglBase.eglBaseContext, true, true))
+            .setOptions(PeerConnectionFactory.Options().apply {
+                disableEncryption = true
+                disableNetworkMonitor = true
+            })
+            .createPeerConnectionFactory()
+
+        // Setup video view object.
+        val videoViewer = binding.videoView
+        videoViewer.setMirror(true)
+        videoViewer.setEnableHardwareScaler(true)
+        videoViewer.init(rootEglBase.eglBaseContext, null)
+
+        val capturer = Camera2Enumerator(this).run {
+            deviceNames.find {
+                isFrontFacing(it)
+            }?.let {
+                createCapturer(it, null)
+            } ?: throw IllegalStateException()
+        }
+
+        val localVideoSource = peerConnectionFactory.createVideoSource(false)
+
+        val surfaceTextureHelper = SurfaceTextureHelper.create(Thread.currentThread().name, rootEglBase.eglBaseContext)
+        (capturer as VideoCapturer).initialize(surfaceTextureHelper, this, localVideoSource.capturerObserver)
+
+        // width, height, frame per second
+        capturer.startCapture(320, 240, 60)
+
+        val localVideoTrack = peerConnectionFactory.createVideoTrack("101", localVideoSource)
+        localVideoTrack.addSink(video_view)
     }
 }
