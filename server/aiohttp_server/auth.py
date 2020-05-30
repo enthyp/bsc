69b8c4c1cd59a3fa83c62665031665208da22028
Jakub Łanecki <jakub.lanecki@relia-sol.pl>
2020-05-30 20:01:17 +0200

    Started work on authentication.

---
+++ server/aiohttp_server/auth.py
@@ -0,0 +1,30 @@
+import bcrypt
+from aiohttp_security.abc import AbstractAuthorizationPolicy
+
+
+class DBAuthorizationPolicy(AbstractAuthorizationPolicy):
+    def __init__(self, storage):
+        self.storage = storage
+
+    async def authorized_userid(self, identity):
+        registered = await self.storage.registered(identity)
+        if registered:
+            return identity
+        else:
+            return None
+
+    async def permits(self, identity, permission, context=None):
+        if identity is None:
+            return False
+        registered = await self.storage.registered(identity)  # none are privileged o.O
+        return registered
+
+
+async def check_credentials(storage, username, password):
+    password_hash = await storage.get_hash(username)
+
+    if password_hash:
+        if bcrypt.checkpw(password.encode(), password_hash.encode()):
+            return True
+
+    return False
---
+++ server/aiohttp_server/storage.py
@@ -0,0 +1,64 @@
+import aiopg.sa as aiosa
+import sqlalchemy as sa
+
+meta = sa.MetaData()
+users = sa.Table(
+    'users', meta,
+    sa.Column('id', sa.Integer, primary_key=True),
+    sa.Column('login', sa.String(100), unique=True, nullable=False),
+    sa.Column('password', sa.String(100), nullable=False),
+    sa.Column('token', sa.String(100), nullable=False),
+)
+
+create_user_table = ('CREATE TABLE IF NOT EXISTS users('
+                     'id SERIAL PRIMARY KEY, '
+                     'login VARCHAR (100) UNIQUE NOT NULL, '
+                     'passwd VARCHAR (500) NOT NULL);')
+
+
+class DBStorage:
+    def __init__(self, db):
+        self.db = db
+
+    async def close(self):
+        self.db.close()
+        await self.db.wait_closed()
+
+    async def registered(self, login):
+        async with self.db.acquire() as conn:
+            s_query = users.count().where(users.c.login == login)
+            user_res = await conn.scalar(s_query)
+            if user_res:
+                return True
+            return False
+
+    async def get_hash(self, login):
+        async with self.db.acquire() as conn:
+            s_query = users.select().where(users.c.login == login)
+            user_res = await conn.execute(s_query)
+            if user_res:
+                user = await user_res.fetchone()
+                if user:
+                    return user['password']
+            return None
+
+
+async def get_engine(config):
+    return await aiosa.create_engine(config.db_url)
+
+
+async def cleanup_storage(app):
+    await app['storage'].close()
+
+
+def setup_db(app, config):
+    async def _setup(app):
+        engine = await get_engine(config)
+        app['storage'] = Storage(engine)
+
+        # Create tables.
+        async with engine.acquire() as conn:
+            await conn.execute(create_user_table)
+
+    app.on_startup.append(_setup)
+    app.on_cleanup.append(cleanup_storage)
